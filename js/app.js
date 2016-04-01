96ebba400ca62f119f1b46d1284a9ec8d980c673
bhennes2 <bhennes2@gmail.com>
2016-04-01 14:57:23 -0500

    Updated delete logic

--- js/app.js
+++ js/app.js
@@ -5,15 +5,18 @@ app.controller('MainCtrl', function($scope){
   $scope.items = [];
   $scope.item = {};
 
+  $scope.today = new Date();
+
   $scope.addItem = function(e, item){
     e.preventDefault();
-    $scope.items.push(item);
-    $scope.item = {};
+    if (item.name.length > 0){
+      $scope.items.push(item);
+      $scope.item = {};
+    }
   };
 
   $scope.removeItem = function(e, index){
-    var items = $scope.items.splice(index, 1);
-    $scope.items = items;
+    $scope.items.splice(index, 1);
   };
 
 });
